342bc75a45d983ea03dab3ddb8fdb4cf5adb414b
appcreatorguy <manmenonsense@gmail.com>
2018-08-26 17:03:55 +0100

    Added player and title screen selections

---
+++ game.py
@@ -0,0 +1,39 @@
+# Python Text RPG
+# Manas Mengle
+
+import cmd
+import textwrap
+import sys
+import os
+import time
+import random
+
+screen_width = 100
+
+##### Player Setup #####
+class player:
+    def __init__(self):
+        self.name = ''
+        self.hp = 0
+        self.mp = 0
+        self.status_effects = []
+my_player = player()
+
+##### Title Screen #####
+def title_screen_selections():
+    option = input("> ")
+    if option.lower() == ("play"):
+        start_game() # placeholder until written
+    elif option.lower() == ("help"):
+        help_menu()
+    elif option.lower() == ("quit"):
+        sys.exit()
+    while option.lower() not in ['play', 'help', 'quit']:
+        print("Please enter a valid command.")
+        option = input("> ")
+        if option.lower() == ("play"):
+            start_game() # placeholder until written
+        elif option.lower() == ("help"):
+            help_menu()
+        elif option.lower() == ("quit"):
+            sys.exit()
